71e933cb02959bbd12d26a01224fc5e07c32d4d8
lucasg <lucas.georges@outlook.com>
2015-07-16 10:54:47 +0200

    Fix code style
    The challenge [01] 's code source wasn't really compliant with standard
    style code.
    Fixes :
     - Use of defines instead of magic numbers
     - Use of standard files handlers identifiers

--- [01]_base64/main.c
+++ [01]_base64/main.c
@@ -1,20 +1,30 @@
-#include "../tools/b64.h"
+#include "b64.h"
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdint.h>
 
+/*
+ * There is a 0.75 decompression ration between b64 and ascii encodings
+ */ 
+#define ASCII_WORDCOUNT (3)
+#define B64_WORDCOUNT 	(4)
 
-// Usage : b64enc.EXEC < input > output
+
+/* 
+ * Usage : b64enc.EXEC < input > output
+ */
 int main(int argc, char *argv[])
 {
-	char ascii[3]= {0}, b64[4]= {0};
-	while (read(0, ascii, 3))
+	char ascii[ASCII_WORDCOUNT]= {0},
+		 b64[B64_WORDCOUNT] = {0};
+
+	while (read(STDIN_FILENO, ascii, ASCII_WORDCOUNT))
 	{
 
-		b64_encode(b64, ascii, 3);
+		b64_encode(b64, ascii, ASCII_WORDCOUNT);
 
-		write(1, b64, 4);
+		write(STDOUT_FILENO, b64, B64_WORDCOUNT);
 
 
 		memset(b64,0, sizeof(b64));
